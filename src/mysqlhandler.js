e8f75010ed37484297714b6337894d9ef3f57aab
Francesco Nwokeka <francesco.nwokeka@rawfish.it>
2017-11-14 20:19:31 +0100

    Using promises to add player kill
    Had to use promises in order to correctly:
    * check redis db to see if player cache was present
    * if not, query the database and update the cache
    * do this for both killer and victim
    * finally update the killcount table

--- src/mysqlhandler.js
+++ src/mysqlhandler.js
@@ -25,30 +25,164 @@ redisClient.on('connect', function() {
 });
 
 
+process.on(
+    "unhandledRejection",
+    function handleWarning( reason, promise ) {
 
-function addPlayer(playerName) {
-    console.log("[AddPlayer]");
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            console.log("");
-        } else {
-            // TODO check that both players are in the db before creating this relationship
-            // add the player to our database
-            connection.query(mysql.format("insert into aliases (name) values(?)", [playerName]), function(error, results, fields) {
-                if (error) {
-                    console.log("ERR- " + error);
-                } else {
-                    connection.release();
-                    console.log("[Mysqlhandler::addPlayer] player added with id: " + results.insertId );
+        console.log( "[PROCESS] Unhandled Promise Rejection" ) ;
+        console.log( "- - - - - - - - - - - - - - - - - - -" ) ;
+        console.log( reason );
+        console.log(  "- -" );
 
-                    // add alias id to the redis db
-                    redisClient.set(playerName, results.insertId);
-                }
-            });
-        }
     });
+
+
+
+
+/** returns a promise with a check for the key on the redis database */
+function getRedisData(key) {
+    var promise = new Promise(function(resolve, reject) {
+        redisClient.get(key, function(err, reply) {
+            if (reply == null) {
+                reject(Error("Key " + key + " not present in redis database"));
+            } else {
+                resolve(reply);
+            }
+        });
+    });
+    return promise;
 }
 
+/** returns a promise for a query on the sql database */
+function executeSqlQuery(query) {
+    var promise = new Promise(function(resolve, reject) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                reject(Error("[Mysqlhandler::executeSqlQuery] ERROR: " + err));
+            } else {
+                console.log("[Mysqlhandler::executeSqlQuery] executing: " + query);
+                connection.query(query, function (error, results, fields) {
+                    if (err) {
+                        reject(Error("[Mysqlhandler::executeSqlQuery] ERROR: " + error));
+                    } else {
+                        connection.release();
+                        console.log("[Mysqlhandler::executeSqlQuery] COUNT: " + results.length);
+                        resolve(results);
+                    }
+                });
+            }
+        });
+    });
+
+    return promise;
+}
+
+/**
+ * adds a player to the sql database and uses the insert id to cache it into redis
+ *
+ * @return the id of the inserted player
+ */
+function addPlayer(playerName) {
+    var promise = new Promise(function(resolve, reject) {
+        console.log("[AddPlayer]");
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                reject(Error("[Mysqlhandler::addPlayer] ERROR: " + err));
+            } else {
+                // add the player to our database
+                executeSqlQuery(mysql.format("insert into aliases (name) values(?)", [playerName])).then(function(data) {
+                    console.log("[Mysqlhandler::addPlayer] player added with id: " + data.insertId);
+                    connection.release();
+
+                    // add alias id to the redis db
+                    redisClient.set(playerName, data.insertId);
+                    resolve(data.insertedId);
+                }, function(error) {
+                    reject(error);
+                });
+            }
+        });
+    });
+
+    return promise;
+}
+
+/**
+ * Pulls information from the database and stash it to redis if available
+ *
+ * @return the id of the requested player
+ */
+function getPlayerFromDatabase(playerName) {
+    var promise = new Promise(function(resolve, reject) {
+        // pull data from redis db
+        getRedisData(playerName).then(function(data) {
+            console.log("[getPlayerFromDatabase] REDIS RETURNED -> " + data);
+            resolve(data);
+        }, function(error) {
+            console.log("[getPlayerFromDatabase] REDIS ERRORED -> " + error);
+
+            // lookup the database to see if the player name is stashed there
+            executeSqlQuery(mysql.format("select id from aliases where name=?", [playerName])).then(function(data) {
+                if (data.length > 0) {
+                    // set the data in the redis db
+                    redisClient.set(playerName, data[0].id);
+                    resolve(data[0].id);
+                } else {
+                    reject(null);
+                }
+            }, function(error) {
+                console.log("[getPlayerFromDatabase] ERROR: " + error);
+                reject(Error(error));
+            });
+        });
+    });
+
+    return promise;
+}
+
+
+function checkPlayerExistance(playerName) {
+    var promise = new Promise(function(resolve, reject) {
+        getPlayerFromDatabase(playerName).then(function(data) {
+            console.log("YAY -> " + data);
+            resolve(data);
+        }, function(error) {
+            // add the alias so we can use it for the killcount table
+            addPlayer(playerName).then(function(data) {
+                console.log("PLAYER CORRECTLY ADDED -> " + data);
+                resolve(data);
+            }, function(error) {
+                console.log("THERE WAS A PROBLEM: " + error);
+                reject(error);
+            });
+        });
+    });
+
+    return promise;
+}
+
+// function addPlayer(playerName) {
+//     console.log("[AddPlayer]");
+//     pool.getConnection(function (err, connection) {
+//         if (err) {
+//             console.log("");
+//         } else {
+//             // TODO check that both players are in the db before creating this relationship
+//             // add the player to our database
+//             connection.query(mysql.format("insert into aliases (name) values(?)", [playerName]), function(error, results, fields) {
+//                 if (error) {
+//                     console.log("ERR- " + error);
+//                 } else {
+//                     connection.release();
+//                     console.log("[Mysqlhandler::addPlayer] player added with id: " + results.insertId );
+//
+//                     // add alias id to the redis db
+//                     redisClient.set(playerName, results.insertId);
+//                 }
+//             });
+//         }
+//     });
+// }
 
 /** check if the player is already stored on the database. If not, add it */
 function playerExists(playerName) {
@@ -86,36 +220,56 @@ function playerExists(playerName) {
 }
 
 function playerKill(jsonData) {
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            console.log("[Mysqlhandler::playerKill] ERROR: " + err);
-        } else {
-            connection.query(mysql.format("select id from killcount where killer=? and victim=?", [jsonData["killer_guid"], jsonData["victim_guid"]]), function (error, results, fields) {
-                if (err) {
-                    console.log("[Mysqlhandler::playerKill] ERROR: " + error);
-                } else {
-                    console.log("COUNT: " + results.length);
+    // We check that the alias ids are in the db and stashed to redis before adding the kill record
+    let killerId;
+    let victimId;
 
-                    if (results.length == 0) {
-                        // create the new relationship
-                        connection.query(mysql.format("insert into killcount (killer, victim, kills, ??) values (?, ?, 1, 1)", [jsonData["body_part"], jsonData["killer_guid"], jsonData["victim_guid"]]), function (error, results, fields) {
-                            if (error) {
-                                console.log("[Mysqlhandler::playerKill] Insert error: " + error);
-                            }
-                            connection.release();
-                        });
-                    } else {
-                        // update stats
-                        connection.query(mysql.format("update killcount set kills=kills+1, ??=??+1 where killer=? and victim=?", [jsonData["body_part"], jsonData["body_part"], jsonData["killer_guid"], jsonData["victim_guid"]]), function (error, results, fields) {
-                            if (error) {
-                                console.log("[Mysqlhandler::playerKill] ERROR: " + error);
-                            }
-                            connection.release();
-                        });
-                    }
+    // Killer
+    checkPlayerExistance(jsonData["killer_name"]).then(function(data) {
+        console.log("Player in db");
+        killerId = data;
+
+        // Victim
+        checkPlayerExistance(jsonData["victim_name"]).then(function(data) {
+            console.log("Player in db");
+            victimId = data;
+
+            console.log("RETURNED VALUES: " + killerId + " - " + victimId);
+
+            // does a record already exist?
+            executeSqlQuery(mysql.format("select id from killcount where killer=? and victim=?", [killerId, victimId])).then(function(data) {
+                console.log("KILL RECORD EXISTS! " + data);
+
+                if (data != 0) {
+                    // add kill record to db
+                    executeSqlQuery(mysql.format("update killcount set kills=kills+1, ??=??+1 where id=?", [jsonData["body_part"], jsonData["body_part"], data[0].id])).then(function(data) {
+                        console.log("KILL RECORD CORRECTLY ADDED");
+                        return true;
+                    }, function(error) {
+                        console.log("FUCK " + error);
+                        return false;
+                    });
+                } else {
+                    // add kill record to db
+                    executeSqlQuery(mysql.format("insert into killcount (killer, victim, kills, ??) values (?, ?, 1, 1)", [jsonData["body_part"], killerId, victimId])).then(function(data) {
+                        console.log("KILL RECORD CORRECTLY ADDED");
+                        return true;
+                    }, function(error) {
+                        console.log("FUCK " + error);
+                        return false;
+                    });
                 }
+            }, function(error) {
+                console.log("ERROR: ! " + error);
+                return false;
             });
-        }
+        }, function(error) {
+            console.log("Can't add player to db");
+            return false;
+        });
+    }, function(error) {
+        console.log("Can't add player to db");
+        return false;
     });
 }
 
@@ -133,6 +287,6 @@ module.exports = {
     playerKilled: function (data) {
         // we receive the follogin json
         // {"type":"K","victim_guid":"929642","victim_name":"Tegamen","killer_guid":"705473","killer_name":"Sbiego","body_part":"head"}
-        playerKill(data);
+        console.log("PLAYER KILL REGISTERED LOG: " + playerKill(data));
     }
 }
